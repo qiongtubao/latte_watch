f4602e20b32b085254fa624bd6d0baba01d61ea4
qiongtubao <810776886@qq.com>
2015-08-27 09:05:59 +0800

    update watcher

--- index.js
+++ index.js
@@ -3,9 +3,14 @@
       缺点: 在ready 之后close   好像会阻塞http
     */
     this.create = function(path) {
+        return require("./lib/fs.js").create(path);
+    }
+    /**
+    this.create = function(path) {
         var Watcher = require("./lib/index.js");
         return  new Watcher(path);
     }
+    */
     this.createFiles = function() {
         var Watcher = require("./lib/files.js");
         var watcher =  new Watcher();
